ae172a3650dc163021e78a5b53eabe640c22d64f
MrMike1234 <mike.guman@radiancetech.com>
2016-08-01 12:42:38 -0500

    test commit

---
+++ MikesTimeCalc/frmMain.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace MikesTimeCalc
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.mikesWidget1 = new MikesWidget.MikesWidget();
+            this.mikesWidget2 = new MikesWidget.MikesWidget();
+            this.mikesWidget3 = new MikesWidget.MikesWidget();
+            this.mikesWidget4 = new MikesWidget.MikesWidget();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "&Calc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Start";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Stop";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Stop";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Start";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Total";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.30303F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60.60606F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.mikesWidget1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.mikesWidget2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.mikesWidget3, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.mikesWidget4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtResult, 1, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(33, 21);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(239, 175);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtResult.Location = new System.Drawing.Point(94, 147);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 14;
+            // 
+            // mikesWidget1
+            // 
+            this.mikesWidget1.Data = "";
+            this.mikesWidget1.Location = new System.Drawing.Point(75, 3);
+            this.mikesWidget1.Name = "mikesWidget1";
+            this.mikesWidget1.Size = new System.Drawing.Size(138, 29);
+            this.mikesWidget1.TabIndex = 10;
+            // 
+            // mikesWidget2
+            // 
+            this.mikesWidget2.Data = "";
+            this.mikesWidget2.Location = new System.Drawing.Point(75, 38);
+            this.mikesWidget2.Name = "mikesWidget2";
+            this.mikesWidget2.Size = new System.Drawing.Size(138, 29);
+            this.mikesWidget2.TabIndex = 11;
+            // 
+            // mikesWidget3
+            // 
+            this.mikesWidget3.Data = "";
+            this.mikesWidget3.Location = new System.Drawing.Point(75, 73);
+            this.mikesWidget3.Name = "mikesWidget3";
+            this.mikesWidget3.Size = new System.Drawing.Size(138, 29);
+            this.mikesWidget3.TabIndex = 12;
+            // 
+            // mikesWidget4
+            // 
+            this.mikesWidget4.Data = "";
+            this.mikesWidget4.Location = new System.Drawing.Point(75, 108);
+            this.mikesWidget4.Name = "mikesWidget4";
+            this.mikesWidget4.Size = new System.Drawing.Size(138, 29);
+            this.mikesWidget4.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(151, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 36);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "&Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 253);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TimeCalc";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private MikesWidget.MikesWidget mikesWidget1;
+        private MikesWidget.MikesWidget mikesWidget2;
+        private MikesWidget.MikesWidget mikesWidget3;
+        private MikesWidget.MikesWidget mikesWidget4;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MikesTimeCalc/frmMain.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MikesTimeCalc
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            LoadFormData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.mikesWidget1.IsValid &&
+                this.mikesWidget2.IsValid &&
+                this.mikesWidget3.IsValid &&
+                this.mikesWidget4.IsValid )
+               
+            {
+                TimeSpan ts = this.mikesWidget2.DateTimeData - this.mikesWidget1.DateTimeData +
+                             (this.mikesWidget4.DateTimeData - this.mikesWidget3.DateTimeData);
+
+                txtResult.Text = String.Format("{0}:{1}", ts.Hours, ts.Minutes);
+            }else{
+                MessageBox.Show("Please correct time values.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FormData.Instance["mikeswidget1"] = this.mikesWidget1.Data;
+            FormData.Instance["mikeswidget2"] = this.mikesWidget2.Data;
+            FormData.Instance["mikeswidget3"] = this.mikesWidget3.Data;
+            FormData.Instance["mikeswidget4"] = this.mikesWidget4.Data;
+            //
+            FormData.Instance.Save();
+            
+            
+
+             this.Dispose();
+        }
+
+
+        private void LoadFormData()    
+        {
+            FormData.Instance.Load();
+            this.mikesWidget1.Data = FormData.Instance["mikeswidget1"];
+            this.mikesWidget2.Data = FormData.Instance["mikeswidget2"];
+            this.mikesWidget3.Data = FormData.Instance["mikeswidget3"];
+            this.mikesWidget4.Data = FormData.Instance["mikeswidget4"];
+        }
+    }//eoc
+}//eon
